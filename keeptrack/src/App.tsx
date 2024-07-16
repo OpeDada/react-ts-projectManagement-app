edd917a3b0b6c97d3787c7ca2a3ea890cfeaa48c
OpeDada <opedada@gmail.com>
2024-07-16 16:12:11 +0900

    starter files

---
+++ keeptrack/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import ProjectPage from './projects/ProjectPage';
+
+function App() {
+  return (
+<div className='container'>
+
+  <ProjectPage />
+</div>
+  );
+}
+
+export default App;
