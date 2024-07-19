cf7311e59c2e65888bd33c5dc86e122d044aa725
OpeDada <opedada@gmail.com>
2024-07-19 13:01:41 +0900

    implemented list of projects cmponent

---
+++ src/projects/ProjectList.tsx
@@ -0,0 +1,16 @@
+import { Project } from './Project'
+import ProjectCard from './ProjectCard'
+
+interface ProjectListProps {
+  projects: Project[];
+}
+
+export default function ProjectList({ projects }: ProjectListProps){
+  const items = projects.map((project) => (
+        <div key={project.id} className="cols-sm">
+          <ProjectCard project={project}></ProjectCard>
+        </div>
+  ));
+  return <div className='row'>{items}</div>
+
+}
