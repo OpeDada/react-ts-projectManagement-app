e8807962f3340818f956acc950b7a5f2bd52f3b3
OpeDada <opedada@gmail.com>
2024-07-19 13:30:31 +0900

    implemented project form component

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -15,10 +15,21 @@ const { project } = props;
     <form className="input-group vertical">
       <label htmlFor="name">{project.name}</label>
       <input type="text" name="name" placeholder="enter name" />
-      <label htmlFor="description">{formatDescription(project.description)}</label>
 
+      <label htmlFor="description">{formatDescription(project.description)}</label>
       <textarea name="description" id="" placeholder="enter description"></textarea>
+
       <label htmlFor="budget">Budget: {project.budget.toLocaleString()}</label>
+      <input type="number" name="budget" placeholder="enter budget" />
+
+      <label htmlFor="isActive">Active?</label>
+      <input type="checkbox" name="isActive" />
+
+      <div className="input-group">
+        <button className="primary bordered medium">Save</button>
+        <span></span>
+        <button type="button" className="bordered medium">Cancel</button>
+      </div>
     </form>
   )
 }
